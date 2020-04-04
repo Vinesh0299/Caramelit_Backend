1e127cc40029a555f55249642dae491aca07da94
Vinesh <Vinesh.katewa@gmail.com>
2020-04-04 19:04:25 +0530

    Mail is successfully sent on submit

--- flaskserver.py
+++ flaskserver.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, jsonify, request
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+
+app.config.from_pyfile('configmail.cfg')
+mail = Mail(app)
 
 @app.route('/')
 def contact_us():
@@ -8,6 +12,8 @@ def contact_us():
 
 @app.route('/data', methods=['POST'])
 def data():
+    msg = Message(subject="TEst", sender=app.config.get("MAIL_USERNAME"), recipients=["Madarauchiha3524@gmail.com"], body="This is a test mail for" + request.form['name'])
+    mail.send(msg)
     return jsonify(data=[
         request.form['name'],
         request.form['email'],
