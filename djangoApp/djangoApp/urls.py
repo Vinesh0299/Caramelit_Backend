0dbfc6230f7e03b1046ac027426d42c8c35ae973
Vinesh <Vinesh.katewa@gmail.com>
2020-04-06 20:35:22 +0530

    Removed admin and user apps to create loginRegister app that will handle evey login and register queries. Also index page is successfully rendered

--- djangoApp/djangoApp/urls.py
+++ djangoApp/djangoApp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from loginRegister import views as loginviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', loginviews.index, name="index"),
 ]
