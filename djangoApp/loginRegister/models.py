5a8af4fdad4eb2bd2ec98d3c6fbf0d4b33aa45c8
Vinesh <Vinesh.katewa@gmail.com>
2020-04-08 22:26:38 +0530

    Created schema for student/instructor/organisation/college

--- djangoApp/loginRegister/models.py
+++ djangoApp/loginRegister/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class studentUser(models.Model):
+    student_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.BigIntegerField()
+    birth_date = models.DateTimeField()
+    country = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    college = models.CharField(max_length=100)
+    skill_set = models.CharField(max_length=100)
+    profileImg = models.CharField(max_length=100)
+    date_of_reg = models.DateTimeField(default=timezone.now)
+    no_of_courses = models.IntegerField(default=0)
+    password = models.CharField(max_length=100)
+
+class instructor(models.Model):
+    instructor_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.BigIntegerField()
+    subjects = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=8)
+    experience = models.CharField(max_length=100)
+    description = models.TextField()
+    profileImg = models.CharField(max_length=100)
+    date_of_reg = models.DateTimeField(default=timezone.now)
+    password = models.CharField(max_length=100)
+
+class college(models.Model):
+    college_id = models.AutoField(primary_key=True)
+    college_name = models.CharField(max_length=100)
+    university_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.BigIntegerField()
+    country = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    skill_set = models.CharField(max_length=100)
+    description = models.TextField()
+    profileImg = models.CharField(max_length=100)
+    date_of_reg = models.DateTimeField(default=timezone.now)
+    password = models.CharField(max_length=100)
+
+class organisation(models.Model):
+    organisation_id = models.AutoField(primary_key=True)
+    organisation_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.BigIntegerField()
+    description = models.TextField()
+    profileImg = models.CharField(max_length=100)
+    date_of_reg = models.DateTimeField(default=timezone.now)
+    password = models.CharField(max_length=100)
--- djangoApp/loginRegister/views.py
+++ djangoApp/loginRegister/views.py
@@ -1,57 +1,55 @@
 from django.shortcuts import render
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
 
 # Main page
-def index(response):
-    return render(response, 'index.html')
+def index(request):
+    return render(request, 'index.html')
 
 # User related pages
-def user_login(response):
-    return render(response, 'login.html')
+def user_login(request):
+    return render(request, 'login.html')
 
-def user_register(response):
-    return render(response, 'register.html')
+def user_register(request):
+    return render(request, 'register.html')
 
-def user_forgot_password(response):
-    return render(response, 'forgotPassword.html')
+def user_forgot_password(request):
+    return render(request, 'forgotPassword.html')
 
 # Instructor related pages
-def instructor_login(response):
-    return render(response, 'instructor_login.html')
+def instructor_login(request):
+    return render(request, 'instructor_login.html')
 
-def instructor_register(response):
-    return render(response, 'instructor_register.html')
+def instructor_register(request):
+    return render(request, 'instructor_register.html')
 
-def instructor_forgot_password(response):
-    return render(response, 'instructor_forgotPassword.html')
+def instructor_forgot_password(request):
+    return render(request, 'instructor_forgotPassword.html')
 
 # College related pages
-def college_login(response):
-    return render(response, 'college_login.html')
+def college_login(request):
+    return render(request, 'college_login.html')
 
-def college_register(response):
-    return render(response, 'college_register.html')
+def college_register(request):
+    return render(request, 'college_register.html')
 
-def college_forgot_password(response):
-    return render(response, 'college_forgot-password.html')
+def college_forgot_password(request):
+    return render(request, 'college_forgot-password.html')
 
 # Organisation related pages
-def organisation_login(response):
-    return render(response, 'organisation_login.html')
+def organisation_login(request):
+    return render(request, 'organisation_login.html')
 
-def organisation_register(response):
-    return render(response, 'organisation_register.html')
+def organisation_register(request):
+    return render(request, 'organisation_register.html')
 
-def organisation_forgot_password(response):
-    return render(response, 'organisation_forgotPassword.html')
+def organisation_forgot_password(request):
+    return render(request, 'organisation_forgotPassword.html')
 
 # Admin related pages
-def admin_login(response):
-    return render(response, 'admin_login.html')
+def admin_login(request):
+    return render(request, 'admin_login.html')
 
-def admin_register(response):
-    return render(response, 'admin_register.html')
+def admin_register(request):
+    return render(request, 'admin_register.html')
 
-def admin_forgot_password(response):
-    return render(response, 'admin_forgotpassword.html')
+def admin_forgot_password(request):
+    return render(request, 'admin_forgotpassword.html')
