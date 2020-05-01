f7cd6158efc6b6401eedccac013435425e21ef87
Vinesh <Vinesh.katewa@gmail.com>
2020-05-01 10:04:47 +0530

    Created Models for course category, subcategory and course itself

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Course Category
+class Course_category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    category_name = models.CharField(max_length=100)
+    date_of_creation = models.DateTimeField(default=timezone.now)
+
+# Course Subcategory
+class Course_subcategory(models.Model):
+    subcategory_id = models.AutoField(primary_key=True)
+    category = models.ForeignKey(Course_category, on_delete=models.CASCADE)
+    subcategory_name = models.CharField(max_length=100)
+    date_of_creation = models.DateTimeField(default=timezone.now)
+
+# Course
+class Course(models.Model):
+    course_id = models.AutoField(primary_key=True)
+    subcategory = models.ForeignKey(Course_subcategory, on_delete=models.CASCADE)
+    course_name = models.CharField(max_length=100)
+    date_of_creation = models.DateTimeField(default=timezone.now)
+    course_difficulty = models.CharField(max_length=30)
+
+# Course resources
+class course_resource(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    resourse_name = models.CharField(max_length=100)
+    resourse_link = models.CharField(max_length=100)
+    resourse_length = models.CharField(max_length=10)
+    date_of_creation = models.DateTimeField(default=timezone.now)
+    course_difficulty = models.CharField(max_length=30)
